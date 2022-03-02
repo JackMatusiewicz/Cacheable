1ae8a5e90d0a541fd8f5567fc37abb1377448246
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2022-03-02 22:54:40 +0000

    Initial brain dump

---
+++ Function.fs
@@ -0,0 +1,80 @@
+﻿namespace Analytica
+
+open System.Reflection
+open Microsoft.FSharp.Reflection
+open System.Collections.Generic
+open FSharp.Quotations
+open System
+open FSharp.Quotations.Patterns
+
+module Function =
+
+    let invokeStaticMethod (lambda : Expr) (types : Type list) (args : obj list) =
+        let rec invoke lambda =
+            match lambda with
+            | Lambda(_, innerExpr) ->
+                    invoke innerExpr
+            | Call(None, mi, _) ->
+                let mig = mi.GetGenericMethodDefinition ()
+                let genM = mig.MakeGenericMethod(types |> Array.ofList)
+                genM.Invoke(null, args |> List.toArray)
+            | _ ->
+                sprintf "Unable to call a function of type: %s" (lambda.GetType().Name)
+                |> failwith
+        invoke lambda
+
+    let private containsInterface (t : System.Type) (o : obj) =
+        t.ToString().Contains(o.ToString())
+
+    /// Takes a thing of type 'a and tries to make a cachable version.
+    /// If 'a is not a function type, then it just returns the value.
+    /// If 'a is actually a 'b -> 'c, then it will create a cachable version.
+    let rec memoise<'a> (clearSignal : IEvent<unit>) (f : 'a) : 'a =
+        match FSharpType.IsFunction (typeof<'a>) with
+        | false -> f
+        | true ->
+            let (dom, range) = FSharpType.GetFunctionElements typeof<'a>
+            let implementsEquality =
+                let equalityInterface = "System.IEquality"
+                let filter = TypeFilter containsInterface
+                dom.FindInterfaces (filter, equalityInterface)
+                |> fun l -> l.Length > 0
+            if FSharpType.IsFunction dom then
+                invokeStaticMethod
+                    <@ makeCacheFuncWithNonCachedArg @>
+                    [dom; range]
+                    [clearSignal; f]
+                    |> unbox<'a>
+            elif implementsEquality then
+                invokeStaticMethod <@ makeCacheFunc @> [dom; range] [clearSignal; f] |> unbox<'a>
+            else
+                invokeStaticMethod <@ makeObjCacheFunc @> [dom; range] [clearSignal; f] |> unbox<'a>
+
+    /// If the domain type is a function, then there is no meaningful caching we can do.
+    and private makeCacheFuncWithNonCachedArg<'a, 'b> (clearSignal : IEvent<unit>) (f : 'a -> 'b) : 'a -> 'b =
+        let v = fun a -> f a |> memoise clearSignal
+        v
+
+    and private makeCacheFunc<'a, 'b when 'a : equality> (clearSignal : IEvent<unit>) (f : 'a -> 'b) : 'a -> 'b =
+        let d = Dictionary<'a, 'b> ()
+        clearSignal.Add (fun _ -> d.Clear())
+
+        fun a ->
+            if d.ContainsKey a then
+                d.[a]
+            else
+                let v = f a |> memoise clearSignal
+                d.[a] <- v
+                v
+
+    and private makeObjCacheFunc<'a, 'b> (clearSignal : IEvent<unit>) (f : 'a -> 'b) : 'a -> 'b =
+        let d = Dictionary<obj, 'b> ()
+        clearSignal.Add (fun _ -> d.Clear())
+
+        fun a ->
+            if d.ContainsKey a then
+                d.[a]
+            else
+                let v = f a |> memoise clearSignal
+                d.[a] <- v
+                v
---
+++ Program.fs
@@ -0,0 +1,58 @@
+﻿open System
+open TypeEquality
+
+/// The caching strategy is inherited by cacheables that merge others.
+/// In these scenarioes LRU will override All.
+[<Struct>]
+type CachingStrategy =
+    | All
+    | LeastRecentlyUsed
+
+type Cacheable<'b,'a> =
+    | Pure of ('b -> 'a) * CachingStrategy
+    | Func of ('b -> 'a) * IEvent<unit> * CachingStrategy
+    | Map of CacheableMap<'b, 'a>
+    | Apply of CacheableApply<'b, 'a>
+    | Contramap of CacheableContramap<'b, 'a>
+    | Bind of CacheableBind<'b, 'a>
+    | PartialApplication of CacheablePartialApplication<'b, 'a>
+
+and CacheableMap<'b, 'a> =
+    abstract Apply<'r> : CacheableMapEval<'b, 'a, 'r> -> 'r
+and CacheableMapEval<'b, 'a, 'r> =
+    abstract Eval<'c> : ('a -> 'c) -> Cacheable<'b, 'a> -> 'r
+
+and CacheableApply<'b, 'a> =
+    abstract Apply<'r> : CacheableApplyEval<'b, 'a, 'r> -> 'r
+and CacheableApplyEval<'b, 'a, 'r> =
+    abstract Eval<'c> : Cacheable<'b, 'a -> 'b> -> Cacheable<'b, 'a> -> 'r
+
+and CacheableContramap<'b, 'a> =
+    abstract Apply<'r> : CacheableContramapEval<'b, 'a, 'r> -> 'r
+and CacheableContramapEval<'b, 'a, 'r> =
+    abstract Eval<'c> : ('c -> 'b) -> Cacheable<'b, 'a> -> 'r
+
+and CacheableBind<'b, 'a> =
+    abstract Apply<'r> : CacheableBindEval<'b, 'a, 'r> -> 'r
+and CacheableBindEval<'b, 'a, 'r> = // (a -> m b) -> m a -> m b
+    abstract Eval<'c> : ('c -> Cacheable<'b, 'a>) -> Cacheable<'b, 'c> -> 'r
+
+/// Models the partial application of an argument to a cacheable with a value that is also
+/// cacheable.
+and CacheablePartialApplication<'b, 'a> =
+    abstract Apply<'r> : CacheablePartialApplicationEval<'b, 'a, 'r> -> 'r
+and CacheablePartialApplicationEval<'b, 'a, 'r> =
+    abstract Eval<'c, 'd, 'e, 'f>
+        : Teq<'b, 'c -> 'd>
+        * Teq<'a, 'e -> 'f>
+        * Cacheable<'c, 'd>
+        * Cacheable<'c -> 'd, 'e -> 'f>
+        -> 'r
+
+
+
+[<EntryPoint>]
+let main argv =
+    let message = "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
