4ddc92861f672444511087c8e94e5ca022c947f3
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2022-03-03 11:57:48 +0000

    Adding test project

---
+++ Test/UnitTest1.fs
@@ -0,0 +1,69 @@
+namespace Cacheable.Test
+
+open NUnit.Framework
+open Cacheable
+
+[<TestFixture>]
+module CacheableTest =
+
+    [<Test>]
+    let ``Caching reset works correctly - single function`` () =
+        let mutable ctr = 0
+
+        let add a b =
+            ctr <- ctr + 1
+            a + b
+
+        let ev = Event<unit> ()
+        
+        let memoisedAdd =
+            Cacheable.func add ev.Publish CachingStrategy.All
+            |> Cacheable.convert
+            |> fst
+
+        memoisedAdd 1 2 |> ignore
+        memoisedAdd 1 2 |> ignore
+
+        Assert.That(ctr, Is.EqualTo 1)
+
+        ev.Trigger ()
+        memoisedAdd 1 2 |> ignore
+        memoisedAdd 1 2 |> ignore
+
+        Assert.That(ctr, Is.EqualTo 2)
+
+    [<Test>]
+    let ``Caching reset works correctly - higher order function`` () =
+        let mutable ctr = 0
+
+        let add a b =
+            ctr <- ctr + 1
+            a + b
+
+        let mutable ctr2 = 0
+
+        let double (f : int -> int -> int) a =
+            ctr2 <- ctr2 + 1
+            f a a
+
+        let ev = Event<unit> ()
+
+        let liftedDouble = Cacheable.lift double CachingStrategy.All
+
+        let (memoisedAdd, finalEv) =
+            Cacheable.func add ev.Publish CachingStrategy.All
+            |> Cacheable.applyArg liftedDouble
+            |> Cacheable.convert
+
+        memoisedAdd 1 |> ignore
+        memoisedAdd 1 |> ignore
+        
+        Assert.That(ctr, Is.EqualTo 1)
+        Assert.That(ctr2, Is.EqualTo 1)
+
+        ev.Trigger ()
+        memoisedAdd 1 |> ignore
+        memoisedAdd 1 |> ignore
+
+        Assert.That(ctr, Is.EqualTo 2)
+        Assert.That(ctr2, Is.EqualTo 2)
