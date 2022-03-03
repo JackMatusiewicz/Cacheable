2bf1d41aca07c64f079ad29f64a1f774d4591e1e
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2022-03-03 09:30:28 +0000

    Adding a toy example

---
+++ Example.fs
@@ -0,0 +1,22 @@
+﻿namespace Cacheable
+
+module Example =
+
+    let addNumbers (a : int) (b : int) : int = a + b
+
+    let applyTwice (f : int -> int -> int) a = f a a
+
+    let addNumbersCacheable =
+        let ev = Event<unit> ()
+        ev.Trigger, Cacheable.func addNumbers ev.Publish CachingStrategy.All
+
+    let applyTwiceCacheable =
+        let ev = Event<unit> ()
+        ev.Trigger, Cacheable.func applyTwice ev.Publish CachingStrategy.All
+
+    let mergedCacheable =
+        let (_, add) = addNumbersCacheable
+        let (_, big) = applyTwiceCacheable
+        let merged = Cacheable.applyArg big add
+        merged
+
